c91276aaaeb5750b9616cb57dcbba3912587c4b0
sipproject <57045737+sipproject@users.noreply.github.com>
2020-09-30 20:52:43 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -4,9 +4,12 @@ using namespace std;
  
 int main()
 {
+   char string[256];   
  
   cout<<"Please enter a long string: ";
   cin.getline ( string, 256, '\n' );              // Input goes into string
   cout<<"Your long string was: "<< string <<endl;
   cin.get();
 }
+
+
