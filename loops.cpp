47efe8fedcffcd8439bedc677517fc1db70667c0
vvviiitttaaalll123 <71178672+vvviiitttaaalll123@users.noreply.github.com>
2020-09-30 20:41:22 +0530

    Update loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -6,9 +6,5 @@ int main()
 { 
   int x = 0;  // Don't forget to declare variables
    
-  while ( x < 10 ) { // While x is less than 10 
-    cout<< x <<endl;
-    x++;             // Update x so the condition can be met eventually
-  }
   cin.get();
 }
